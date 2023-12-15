31484f6ffcfc5022214925f2beefaf1d25b896b7
SanzayKdg <sanjaykhadgi9861@gmail.com>
2023-12-15 18:52:58 +0545

    portfolio fiexd

--- src/Components/Portfolio/project.ts
+++ src/Components/Portfolio/project.ts
@@ -1,16 +1,23 @@
+import image1 from "./images/project1.png";
+import image2 from "./images/project2.png";
+
 export const projects = [
   {
     name: "Hamro Jobs",
-    image: "image.jpg",
+    image: image2,
     description:
       "Hamro Job website is an online platform that allows job seekers to apply for jobs opportunities and employer to post job for different positions. The website provides an interface for searching, applying, and posting jobs.",
     tools: ["MongoDB", "ExpressJS", "ReactJS", "NodeJS"],
+    demo__link: "#demo",
+    code__link: "#code",
   },
   {
     name: "BizHub",
-    image: "image.jpg",
+    image: image1,
     description:
       "A business directory website for businesses to showcase what they offer & for customers to easily find what they need. Businesses, big or small, can list their services. A simple solution to connect businesses with customers.",
     tools: ["Nest", "NextJS", "PostgreSQL"],
+    demo__link: "#demo",
+    code__link: "#code",
   },
 ];
